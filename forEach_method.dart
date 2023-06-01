a855b29f5028f07f033424e5029ccc277ae554bb
ClementPeter <61213263+ClementPeter@users.noreply.github.com>
2023-06-01 13:12:56 +0100

    Update forEach_method.dart

--- forEach_method.dart
+++ forEach_method.dart
@@ -1,4 +1,6 @@
-void main() {
+//
+//=====================
+// void main() {
 //   /*PRINT NAMES SEPARATELY USING FOR EACH*/
 //   List<String> names = ["Max", "Peter", "John", "Sarah"];
 
@@ -27,108 +29,98 @@ void main() {
 
 //   /***************************/
 
-  /*FIND THE AGE FOR EACH FRIEND AND PRINT IT USING FOR-EACH*/
-  List<Friend> myFriends = [
-    Friend(name: "Peter", age: 20),
-    Friend(name: "Sara", age: 22),
-    Friend(name: "John", age: 23),
-    Friend(name: "Max", age: 24),
-  ];
-  
-  myFriends.forEach((friends){
-   print("${friends.name} is ${friends.age} y/o");    
-  });
-  
+//   /*FIND THE AGE FOR EACH FRIEND AND PRINT IT USING FOR-EACH*/
+//   List<Friend> myFriends = [
+//     Friend(name: "Peter", age: 20),
+//     Friend(name: "Sara", age: 22),
+//     Friend(name: "John", age: 23),
+//     Friend(name: "Max", age: 24),
+//   ];
 
-  /***************************/
-}
-
-class Friend {
-  String? name;
-  int? age;
-  Friend({this.name, this.age});
-}
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-void main() {
-  List<String> names = ['John', 'Jane', 'Jim'];
-
-  /////////////////
-
-  names.forEach((name) {
-    name = name.toUpperCase();
-    print("Inside using FOR EACH::::::: $name");
-  });
-
-  print("\n");
-  print("Outside using FOR EACH::::::: $names");
-
-  ////////////////
-  print("\n");
-  print(":::::::USING FOR LOOP::::::::::");
-  //
-  for (int i = 0; i < names.length; i++) {
-    names[i] = names[i].toUpperCase();
-    //print(names[i].toUpperCase());
-  }
-  print("using FOR LOOP::::::: $names");
-
-  //////////////////
-  print("\n");
-  print(":::::::USING FOR-IN LOOP::::::::::");
-
-  for (String name in names) {
-    //upName = name.toUpperCase();
-    print(name.toUpperCase());
-    print("\n");
-  }
-}
-
-
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//PRINT EVEN NUMBERS USING .where
-void main() {
-  List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  
-    List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    numbers.where((number) => number % 2 == 0).forEach((number) {
-    print(number);
-  });
-  
-}
-
-
-//Advanced example: Using forEach with async and await
-
-// void main() {
-//   Future printDelayedMessage(int delay, String message) async {
-//    await Future.delayed(const Duration(seconds: 1));
-//     print(message);
-//   }
-
-//   List<String> messages = ['message 1', 'message 2', 'message 3'];
-
-//   Future.forEach(messages, (message) async {
-//     await printDelayedMessage(1, message);
+//   myFriends.forEach((friends){
+//    print("${friends.name} is ${friends.age} y/o");
 //   });
+
+//   /***************************/
 // }
 
+// class Friend {
+//   String? name;
+//   int? age;
+//   Friend({this.name, this.age});
+// }
+//=====================
+//
+//
+//
+//
+//=====================
+// void main() {
+//   List<String> names = ['John', 'Jane', 'Jim'];
 
+//   /////////////////
 
+//   names.forEach((name) {
+//     name = name.toUpperCase();
+//     print("Inside using FOR EACH::::::: $name");
+//   });
+
+//   print("\n");
+//   print("Outside using FOR EACH::::::: $names");
+//   print("\n");
+//   print(":::::::USING FOR LOOP::::::::::");
+//   //
+//   for (int i = 0; i < names.length; i++) {
+//     names[i] = names[i].toUpperCase();
+//     print(names[i].toUpperCase());
+//   }
+
+//   //////////////////
+//   print("\n");
+//   print(":::::::USING FOR-IN LOOP::::::::::");
+
+//   for (String name in names) {
+//     //upName = name.toUpperCase();
+//     print(name.toUpperCase());
+//     //print("\n");
+//   }
+// }
+
+//=====================
+//
+//
+//
+//
+//=====================
+//PRINT EVEN NUMBERS USING .where
+// void main() {
+//   List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//   numbers.where((number) => number % 2 == 0).forEach((number) {
+//     print(number);
+//   });
+// }
+//=====================
+//
+//
+//
+//
+//=====================
+
+//Advanced example: Using forEach with async-await, Future to create delay
+
+void main() {
+  Future printDelayedMessage(int delay, String message) async {
+    await Future.delayed(const Duration(seconds: 1));
+    print(message);
+  }
+
+  List<String> messages = ['message 1', 'message 2', 'message 3'];
+
+  Future.forEach(messages, (message) async {
+    await printDelayedMessage(1, message);
+  });
+
+  messages.forEach((message) {
+    print(message);
+  });
+}
