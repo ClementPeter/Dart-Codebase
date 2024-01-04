c67c3a4984d7bae087325e75e8aed00d509cfc0a
ClementPeter <clementpeter2000@gmail.com>
2024-01-04 23:17:16 +0100

    encapsulation

--- zuri_ENCAPSULATION.dart
+++ zuri_ENCAPSULATION.dart
@@ -1,49 +1,89 @@
-// //ENCAPSULATION
-// void main() {
-//   Car car = Car(brand BMW, paint indigo, tyre 4);
+//ENCAPSULATION
+void main() {
+  Car car = Car(brand: 'BMW', paint: 'indigo', tyre: 4);
 
-//   print(car.paint);
-//   print(car.tyre);
-//   print(car._engineType);
+  print(car.paint);
+  print(car.tyre);
+  print(car._engineType);
 
-//   car.ignition();
-//   car.drive();
-//   car.park();
-// }
+  car.ignition();
+  car.drive();
+  car.park();
 
-// ABSTRACT CLASS
-// abstract class Vehicle {
-//   Vehicle({this.wheels, this.color});
+  Trailer trailer = Trailer();
+  trailer.ignition();
+  trailer.drive();
+  trailer.park();
+}
 
-//   String color = black;
-//   int wheels = 4;
+///ABSTRACT CLASS
+abstract class Vehicle {
+  Vehicle({this.wheels, this.color});
 
-//   ignition(); ABSTRACT METHOD
+  String? color = 'black';
+  int? wheels = 4;
 
-//   drive() {
-//     print(Wheels forward);
-//   }
+  ///ABSTRACT METHOD
+  ignition();
 
-//   reverse() {
-//     print(Wheels reverse);
-//   }
+  drive() {
+    print('Wheels forward');
+  }
 
-//   park() {
-//     print(Wheels stop);
-//   }
-// }
+  reverse() {
+    print('Wheels reverse');
+  }
 
-// class Car extends Vehicle {
-//   String brand;
-//   String paint;
-//   int tyre;
-//   String _engineType = 4 cylinder; ENCAPSULATION--- Wont bw accesible to other classes outside
+  park() {
+    print('Wheels stop');
+  }
+}
 
-//   Car({this.brand, this.paint, this.tyre})  super(wheels tyre, color paint);
+//Car extending Vehicle
+class Car extends Vehicle {
+  String? brand;
+  String? paint;
+  int? tyre;
+  String _engineType = '4 cylinder';
 
-//   must override abstract method
-//   @override
-//   ignition() {
-//     print('start Car');
-//   }
-// }
+  ///ENCAPSULATION--- Wont bw accesible to other classes outside
+
+  Car({this.brand, this.paint, this.tyre}) : super(wheels: tyre, color: paint);
+
+  ///must override abstract method
+  @override
+  ignition() {
+    print('start Car');
+  }
+}
+
+//Trailer implementing Vehicle
+class Trailer implements Vehicle {
+  @override
+  String? color;
+
+  @override
+  int? wheels;
+
+  Trailer({this.color, this.wheels});
+
+  @override
+  drive() {
+    print('Drive Trailer');
+  }
+
+  @override
+  ignition() {
+    print('start Trailer');
+  }
+
+  @override
+  park() {
+    print('Park Trailer');
+  }
+
+  @override
+  reverse() {
+    print('Reverse Trailer');
+  }
+}
