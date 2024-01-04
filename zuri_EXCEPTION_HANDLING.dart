1799b3990df6c0760a205ce1467480a996c288c6
ClementPeter <clementpeter2000@gmail.com>
2024-01-04 21:55:36 +0100

    exception_handling

--- zuri_EXCEPTION_HANDLING.dart
+++ zuri_EXCEPTION_HANDLING.dart
@@ -11,81 +11,59 @@
 //   }
 // }
 
-
-
-
-
-// //void main() {
-// // //   try {
-// // //     checkAge(15);
-// // //     checkAge(135);
-// // //   } catch (e) {
-// // //     print(e);
-// // //   } finally {
-// // //     checkAge(135);
-// // //   }
-
-// //   try {
-// //     checkAge(15);
-// //     checkAge(135);
-// //   } on AgeException {
-// //     print(AgeException().error());
-// //   } catch (e) {
-// //     print(e);
-// //   } finally {
-// //     checkAge(135);
-// //   }
-// // }
-
-// // void checkAge(int age) {
-// //   if (age < 18) {
-// //     throw AgeException();
-// //   } else {
-// //     print("Your age bracket is elligble to enter");
-// //   }
-// // }
-
-// // class AgeException implements Exception {
-// //   String error() => "Your age is less than 18, hence illegible to enter";
-// // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//3 usage
 // void main() {
-// //   BankAccount myAcc1 = BankAccount(balance: 20);
-// //   //print(myAcc1.balance);
+//   // try {
+//   //   checkAge(15);
+//   //   checkAge(135);
+//   // } catch (e) {
+//   //   print(e);
+//   // } finally {
+//   //   checkAge(135);
+//   // }
 
-// //   try {
-// //     var myAcc2 = BankAccount.newClient(-100);
-// //     print(myAcc2);
-// //   } catch (e) {
-// //     print(e);
-// //   }
-// // }
+//   try {
+//     checkAge(15);
+//   } on AgeException {
+//     print(AgeException().error());
+//   } catch (e) {
+//     print(e);
+//   } finally {
+//     checkAge(135);
+//   }
+// }
 
-// // class BankAccount {
-// //   double? balance;
-// //   BankAccount({this.balance});
+// //1
+// void checkAge(int age) {
+//   if (age < 18) {
+//     throw AgeException();
+//   } else {
+//     print("Your age bracket is elligble to enter");
+//   }
+// }
 
-// //   BankAccount.newClient(double balance) {
-// //     if (balance < 0) throw Exception('Balance is less than Zero');
-// //   }
-// // }
+// //2
+// class AgeException implements Exception {
+//   String error() => "Your age is less than 18, hence illegible to enter";
+// }
 
+void main() {
+  BankAccount myAcc1 = BankAccount(balance: 20);
+  print(myAcc1.balance);
 
+  try {
+    var myAcc2 = BankAccount.newClient(-100);
+    print(myAcc2);
+  } catch (e) {
+    print(e);
+  }
+}
 
+class BankAccount {
+  double? balance;
+  BankAccount({this.balance});
 
-
+  BankAccount.newClient(double balance) {
+    if (balance < 0) throw Exception('Balance is less than Zero');
+  }
+}
