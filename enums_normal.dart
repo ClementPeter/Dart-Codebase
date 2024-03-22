5c9e3e4a04ce523991952eeb9173713400987e19
ClementPeter <clementpeter2000@gmail.com>
2024-03-22 15:17:28 +0100

    enums

--- enums_normal.dart
+++ enums_normal.dart
@@ -1,39 +1,40 @@
 //Normal Enums
+// enum Direction { north, south, east, west }
+
+// String directionDescription(Direction direction) {
+//   switch (direction) {
+//     case Direction.north:
+//       return 'North';
+
+//     case Direction.south:
+//       return 'South';
+
+//     case Direction.east:
+//       return 'East';
+
+//     case Direction.west:
+//       return 'West';
+//   }
+// }
+
+// void main(List<String> args) {
+//   print(directionDescription(Direction.north));
+//   print(directionDescription(Direction.south));
+// }
+
+//
+//
+//
+//
+//
+//
+
+//Enum Demo 2 - Using extension on enums
+
 enum Direction { north, south, east, west }
 
-String directionDescription(Direction direction) {
-  switch (direction) {
-    case Direction.north:
-      return 'North';
-
-    case Direction.south:
-      return 'South';
-
-    case Direction.east:
-      return 'East';
-
-    case Direction.west:
-      return 'West';
-  }
-}
-
-void main(List<String> args) {
-  print(directionDescription(Direction.north));
-  print(directionDescription(Direction.south));
-}
-
-//
-//
-//
-//
-//
-//
-
-// Enum Demo 2 - Using extension on en
-//enum Direction { north, south, east, west }
-
 extension on Direction {
-  String description() {
+  String get description {
     switch (this) {
       case Direction.north:
         return 'North';
@@ -47,7 +48,7 @@ extension on Direction {
   }
 }
 
-// void main(List<String> args) {
-//   print(Direction.north); //Direction.north
-//   print(Direction.north.description);
-// }
+void main(List<String> args) {
+  print(Direction.north); //Direction.north
+  print(Direction.south.description); //Direction.south
+}
