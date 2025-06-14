1a49361897f2e25f9e8699a20b4392ec9140f966
ClementPeter <clementpeter2000@gmail.com>
2025-06-14 15:36:28 +0100

    gpt_quiz_speakNTimes

---
+++ dart_gpt_coding_quiz/speakNtime.dart
@@ -0,0 +1,30 @@
+class Animal {
+  makeSound() {
+    print("Animal make Sound");
+  }
+}
+
+class Dog extends Animal {
+  @override
+  makeSound() {
+    print("Dog barks");
+  }
+}
+
+class Cat extends Animal {
+  @override
+  makeSound() {
+    print("Cat meows");
+  }
+}
+
+void speakNTimes(int n, Animal animal) {
+  if (n < 1) {
+    throw Exception('');
+  }
+  print(Cat().makeSound());
+}
+
+void main(List<String> args) {
+  speakNTimes(2, Animal());
+}
